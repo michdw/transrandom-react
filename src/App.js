e16568f598530949ab38418b894ca3b40ba8356e
Michael D Weaver <michd.weaver@gmail.com>
2023-09-11 23:40:27 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+
+function App() {
+  const translate = () => {
+    console.log(process.env.NODE_ENV)
+    console.log(process.env.REACT_APP_RapidAPI_Key)
+    const url = "https://text-translator2.p.rapidapi.com/translate";
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Key": process.env.REACT_APP_RapidAPI_Key,
+        "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
+      },
+      body: new URLSearchParams({
+        source_language: "en",
+        target_language: "id",
+        text: "What is your name?",
+      }),
+    };
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="App">
+      <p onClick={translate}>click here</p>
+    </div>
+  );
+}
+
+export default App;
