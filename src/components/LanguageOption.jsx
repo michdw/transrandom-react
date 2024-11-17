9d4a7ed7acc817224eff761b67dca07dc6b5c468
michdw <michd.weaver@gmail.com>
2024-11-17 00:17:55 -0500

    fixed radio selection bug

--- src/components/LanguageOption.jsx
+++ src/components/LanguageOption.jsx
@@ -1,15 +1,18 @@
 import { React } from "react";
 
 export default function LanguageOption(props) {
+
+  function selectThis() {
+    if (props.gamePhase === 2) {
+      props.selectOption(props.option);
+    }
+  }
+
   return (
-    <li className="language-option" key={props.option.code}>
-      <span
-        onClick={() => {
-          if (props.gamePhase === 2) {
-            props.selectOption(props.option);
-          }
-        }}
-      >
+    <li
+      className="language-option"
+      key={props.option.code}
+    >
         <input
           id={props.option.code}
           disabled={props.gamePhase !== 2}
@@ -17,14 +20,19 @@ export default function LanguageOption(props) {
           name="languageOption"
           value={props.option.name}
           className={props.gamePhase === 2 ? "active-radio" : null}
+          onClick={() => {
+            selectThis();
+          }}
         />
         <label
           htmlFor={props.option.code}
           className={props.gamePhase === 2 ? "active-radioLabel" : null}
+          onClick={() => {
+            selectThis();
+          }}
         >
           {props.option.name}
         </label>
-      </span>
     </li>
   );
 }
